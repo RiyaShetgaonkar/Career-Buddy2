323df619fb934325cf36e568e38a9643758160f5
Sheryl Menezes <sherylmenezes12@gmail.com>
2026-01-10 19:36:35 +0530

    Add files via upload
    Courses database

---
+++ coursesData.js
@@ -0,0 +1,33 @@
+// coursesData.js
+module.exports = [
+  {
+    title: "Full Stack Web Development",
+    skills: ["html", "css", "javascript", "react"],
+    degree: ["B.E", "B.Tech"],
+    branch: ["Computer Engineering", "IT", "Computer Science (CSE)"],
+    cgpaMin: 6,
+    platform: "Coursera",
+    link: "https://www.coursera.org/specializations/full-stack-react",
+    image: "https://images.unsplash.com/photo-1519389950473-47ba0277781c"
+  },
+  {
+    title: "Data Science with Python",
+    skills: ["python", "ml", "data science"],
+    degree: ["B.E", "B.Tech"],
+    branch: ["Computer Engineering", "Computer Science (CSE)"],
+    cgpaMin: 7,
+    platform: "Coursera",
+    link: "https://www.coursera.org/professional-certificates/ibm-data-science",
+    image: "https://images.unsplash.com/photo-1555949963-aa79dcee981c"
+  },
+  {
+    title: "NPTEL Programming Course",
+    skills: ["c", "java"],
+    degree: ["Any"],
+    branch: ["Any"],
+    cgpaMin: 0,
+    platform: "NPTEL",
+    link: "https://nptel.ac.in/courses",
+    image: "https://images.unsplash.com/photo-1503676260728-1c00da094a0b"
+  }
+];
